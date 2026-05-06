7b6475f93f90a7ee59e715a0b4972f9d853e94fa
PairPear3 <roartepi2103@gmail.com>
2026-05-06 17:11:35 -0400

    Update LinearSearch.java
    #Añadido:
    - Código Añadido

--- searching/LinearSearch.java
+++ searching/LinearSearch.java
@@ -1,1 +1,13 @@
-
+// Algorítmo Linear Search    
+  public static int linearSearch(int[] arr, int target){
+    int index  = 0;
+      for(int i = 0; i < arr.length ; i++){
+          if(arr[i] == target){
+              index = i;
+              break;
+        }else if(i == arr.length - 1 && arr[i] != target){
+            return -1;
+        } 
+      } 
+      return index;
+  } // Fin de Algorítmo
